9e1ab903599067074ee7ad201612409c80e05b6c
G3VS <gayatrivazhattu05@gmail.com>
2026-04-23 16:09:35 +0530

    Test storage backend import

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python -c "from keralakart.storage_backends import CloudinaryStorage; print('Storage backend found OK')"
 python manage.py shell < seed_data.py
